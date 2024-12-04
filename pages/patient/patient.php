3aa870d8cad22ae72503cecdb9493910a4a41a29
Huytai102 <tai.nguyenraphael@hcmut.edu.vn>
2024-12-05 05:10:46 +0700

    Remove redundant session_start() call in patient.php

--- pages/patient/patient.php
+++ pages/patient/patient.php
@@ -2,14 +2,11 @@
 require_once 'core/Database.php';
 require_once 'models/TimeSlot.php';
 
-<<<<<<< HEAD
 if (!isset($_SESSION)) {
     session_start();
 }
 
-=======
 // session_start();
->>>>>>> f505076e11344f0f817f777cd4a378124d9f754a
 if (!isset($_SESSION['userid'])) {
     echo "<script>alert('You must log in as a guest to access this page.'); window.location.href = 'index.php?page=signin';</script>";
     exit();
